975af8cf75cb82dc95d3d5e4976564309bccc60a
Michael Kvak <mkvak1987@gmail.com>
2021-12-21 23:03:20 +0200

    create front

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import List from "../views/List.vue";
+import Create from "../views/Create.vue";
+import Detail from "../views/Detail.vue";
+import Login from "../views/Login.vue";
+import Signup from "../views/Signup.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "List",
+    component: List,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+    path: "/create/:id?",
+    name: "Create",
+    component: Create,
+  },
+  {
+    path: "/detail",
+    name: "Detail",
+    component: Detail,
+  },
+  { path: "/Login", name: "Login", component: Login },
+  { path: "/Signup", name: "Signup", component: Signup },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
+export default router;
 export default router
